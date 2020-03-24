da9ee6fa05b0cb3880b76d74981cf9c4fd81b110
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-24 15:33:57 +0530

    updates

--- main.py
+++ main.py
@@ -24,3 +24,17 @@ class AboutDialog(QDialog):
         title.setFont(font)
 
         layout.addWidget(title)
+
+        logo = QLabel()
+        logo.setPixmap(QPixmap(os.path.join('images', 'ma-icon-128.png')))
+        layout.addWidget(logo)
+
+        layout.addWidget(QLabel("Version 23.35.211.233232"))
+        layout.addWidget(QLabel("Copyright 2015 Mozarella Inc."))
+
+        for i in range(0, layout.count()):
+            layout.itemAt(i).setAlignment(Qt.AlignHCenter)
+
+        layout.addWidget(self.buttonBox)
+
+        self.setLayout(layout)
