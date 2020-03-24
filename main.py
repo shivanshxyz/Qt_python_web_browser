6511b77884be69531b9972b1ea29104753d89569
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-24 15:33:10 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtPrintSupport import *
+
+import os
+import sys
