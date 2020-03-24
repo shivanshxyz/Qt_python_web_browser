e0b4316f29c19a3c1c599a88d3b2131131f68266
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-03-24 15:33:43 +0530

    updates

--- main.py
+++ main.py
@@ -6,3 +6,21 @@ from PyQt5.QtPrintSupport import *
 
 import os
 import sys
+
+class AboutDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(AboutDialog, self).__init__(*args, **kwargs)
+
+        QBtn = QDialogButtonBox.Ok  # No cancel
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        layout = QVBoxLayout()
+
+        title = QLabel("Mozarella Ashbadger")
+        font = title.font()
+        font.setPointSize(20)
+        title.setFont(font)
+
+        layout.addWidget(title)
